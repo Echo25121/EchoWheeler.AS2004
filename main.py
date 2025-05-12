e2c4b3959e2e6501c908e01064d1bd369866095e
Echo25121 <whee25121@wainuiomatahigh.school.nz>
2025-05-13 11:54:35 +1200

    Update main.py

--- main.py
+++ main.py
@@ -7,27 +7,42 @@ Intro = 'Hello and welcome to my quiz!'
 Int_score = 0
 Game_Start = 1
 Int_Num_Question = 0
+Game_end = 0
+count = 0
+
 
 #Where I set Questions
 questions = [
-    ["What mammal doesn't lay eggs", [
-        ["(1) Horse"],
-        ["(2) Long Beaked Echidna"],
-        ["(3) Short Beaked Echidna"],
-        ["(4) Platypus"]
+    ["What does Gandalf the Grey say to the Balrog to protection the fellowship", [
+        ["(1) You shall not pass!"],
+        ["(2) please stay there"],
+        ["(3) Die foul beast!"],
+        ["(4) I will kill you"]
     ], "1"],
-    ["Which animal was recently claimed by scientests to have had a de-extinction", [
-        ["(1) Wooly Mammoth"],
-        ["(2) Dire Wolf"],
-        ["(3) Velociraptor"],
-        ["(4) Tuatara"]
+    ["What is the first lotr movie created called?", [
+        ["(1) Lord of the rings: Journey to mount doom"],
+        ["(2) Lord of the rings: fellowship of the ring"],
+        ["(3) Lord of the rings: return of the king"],
+        ["(4) The hobbit"]
     ], "2"],
-    ["What modern day animal has the strongest biteforce", [
-        ["(1) Fantail"],
-        ["(2) Tyrannosuarus rex"],
-        ["(3) Golden retriever"],
-        ["(4) Saltwater Crocodile"]
-    ], "4"]
+    ["Who helped Frodo the most", [
+        ["(1) Saruman"],
+        ["(2) Legolas"],
+        ["(3) Dobby"],
+        ["(4) Samwise Gamgee"]
+    ], "4"], 
+    ["What is the spider that Frodo and Samwise Gamgee run into called?", [
+        ["(1) Gleeok"],
+        ["(2) Helob"],
+        ["(3) Ungoliath"],
+        ["(4) Shelob"]
+    ], "4"],
+    ["Who dies at the end due to their love of the ring?", [
+        ["(1) Gollum"],
+        ["(2) Bilbo Baggins"],
+        ["(3) Frodo"],
+        ["(4) Dobby"]
+    ], "1"]
 ]
 
 # Function for Questions
@@ -52,7 +67,7 @@ def askQuestion(name, answers, Cor_answer,Int_score):
     print ('The Answer is ' + Cor_answer)
    
     return Int_score
-
+#Game running
 while Game_Start == 1:
     Game_Start = 0
     print(Intro)
@@ -61,21 +76,29 @@ while Game_Start == 1:
     for question in questions:
         Int_score = askQuestion(question[0], question[1], question[2], Int_score)
     print ("Your score is " + str(Int_score))
-    if Int_score == 3:
+    if Int_score == 5:
         print('Congratulations you got all of them right!')
     print("Do you want to try again? Y/N") 
     Int_score = 0
-    Str_Continue = input("")
-    if Str_Continue == "Y" or Str_Continue == "y":
-        Game_Start = 1
-    elif Str_Continue == "N" or Str_Continue == "n":
-        print("Good bye")
-        quit()
-    else:
-        print("Please choose Y or N")
+    #Game Ending
+    Game_end = 1
+    while Game_end == 1:
+        Game_end = 0
+        Str_Continue = input("")
+        if Str_Continue == "Y" or Str_Continue == "y":
+            Game_Start = 1
+        elif Str_Continue == "N" or Str_Continue == "n":
+            print("Good bye")
+            quit()
+        else:
+            print("Please choose Y or N")
+            Game_end = 1
+            count += 1
+
+
         
         
         
         
-   
+        
         
